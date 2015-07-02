414f3a069879ac4feef878a2ba38486c4d34347d
Imee Cuison <imee@dynepic.com>
2015-07-01 20:38:27 -0400

    users vc

--- BearsApp3/UsersViewController.swift
+++ BearsApp3/UsersViewController.swift
@@ -9,26 +9,104 @@
 import UIKit
 import CoreBluetooth
 
-class UsersViewController: UIViewController {
 
+var userName = ""
+
+class UsersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var resultsTable: UITableView!
+    
+    var resultsUsernameArray = [String]()
+    var resultsProfileNameArray = [String]()
+    var resultsImageFiles = [PFFile]()
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let theWidth = view.frame.size.width
+        let theHeight = view.frame.size.height
+        
+        resultsTable.frame = CGRectMake(0, 0, theWidth, theHeight - 64)
+        
+        userName = PFUser.currentUser()!.username!
+
+        println("userName")
 
         // Do any additional setup after loading the view.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    override func viewWillAppear(animated: Bool) {
-        self.navigationItem.hidesBackButton = true
- 
-    
+    override func viewDidAppear(animated: Bool) {
+//        
+//                resultsUsernameArray.removeAll(keepCapacity: false)
+//                resultsProfileNameArray.removeAll(keepCapacity: false)
+//                resultsImageFiles.removeAll(keepCapacity: false)
+        
+        let predicate = NSPredicate(format: "username != '"+userName+"'")
+        var query = PFQuery(className: "_User", predicate: predicate)
+        var objects = query.findObjects()
+        
+        for object in objects! {
+            
+            self.resultsUsernameArray.append(object.username!!)
+            self.resultsProfileNameArray.append(object["profileName"] as! String)
+            self.resultsImageFiles.append(object["photo"] as! PFFile)
+            
+            self.resultsTable.reloadData()
+            
+            
+        }
+        
     }
+    
+ 
 
+   override func viewWillAppear(animated: Bool) {
+        self.navigationItem.hidesBackButton = true
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultsUsernameArray.count
+      //  return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:resultsCell = tableView.dequeueReusableCellWithIdentifier("Cell") as! resultsCell
+        
+        cell.usernameLbl.text = self.resultsUsernameArray[indexPath.row]
+        cell.profileNameLbl.text = self.resultsProfileNameArray[indexPath.row]
+        
+        
+        resultsImageFiles[indexPath.row].getDataInBackgroundWithBlock {
+            
+            
+            (imageData: NSData?, error: NSError?) -> Void in
+            
+            if error == nil {
+                let image = UIImage(data: imageData!)
+                cell.profileimg.image = image
+            }
+            
+        }
+        
+        return cell
+    }
+    
+  
+    
+    
     
     
     @IBAction func syncBtn_click(sender: AnyObject) {
